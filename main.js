a326f56e61b94787680bfd21eb68ed1d0827e69b
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-22 14:19:48 +0200

    add: get age method

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("Funzioni?");
+
+class Veicolo {
+    marca;
+    anno;
+    colore;
+    porte;
+    carburante;
+
+    constructor(marca, anno, colore, porte, carburante){
+        this.marca = marca;
+        this.anno = anno;
+        this.colore = colore;
+        this.porte = porte;
+        this.carburante = carburante;
+    }
+
+    calcolaeta(){
+        const currentYear = new Date().getFullYear()
+        return currentYear - this.anno
+    }
+}
+
+const fiat = new Veicolo ("Fiat", 2019, "Blu", 5, "Benzina")
+console.log(fiat.calcolaeta());
+
